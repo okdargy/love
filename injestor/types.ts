dac5420468a5251fbc47f61b72ea82730880fc3e
dargy <hello@dargy.party>
2026-04-07 02:27:37 +0000

    ci: libsql reinstall

--- injestor/types.ts
+++ injestor/types.ts
@@ -99,30 +99,6 @@ export interface Creator {
     thumbnail: string;
 }
 
-	
-// {
-//   "meta": {
-//     "total": 724299,
-//     "perPage": 100,
-//     "currentPage": 1,
-//     "lastPage": 7243,
-//     "firstPage": 1
-//   },
-//   "data": [
-//     {
-//       "rank": 1,
-//       "statistic": 6467086,
-//       "user": {
-//         "id": 14772,
-//         "username": "Everlast",
-//         "membershipID": 2,
-//         "thumbnail": {
-//           "avatar": "https://cdn.polytoria.com/thumbnails/avatars/f3ef8f7c8fd125bae4812bf77047603a6ce78af57d5f9474fe0994a8eeb53cd3.png",
-//           "icon": "https://cdn.polytoria.com/thumbnails/avatars/f3ef8f7c8fd125bae4812bf77047603a6ce78af57d5f9474fe0994a8eeb53cd3-icon.png"
-//         }
-//       }
-//     },
-
 export interface RankingsResponse {
     meta: {
         total: number;
